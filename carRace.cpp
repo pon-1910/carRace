e3b879c4a0b8bcacb432f98a424ee9c14467f997
ikuto_murakami <m.ikuto@icloud.com>
2025-07-21 10:53:23 +0900

    画面遷移と効果音を追加 タイトル画面で左クリックするとプレイ画面に遷移し、燃料(FUEL)が０になると、ゲームオーバー画面へ遷移 タイトル画面とゲームオーバー画面でコンピューターの車が下から上に動く処理を追加 効果音はBGM、コンピューターの車とヒット音、アイテム取得音、ジングルを追加

--- carRace.cpp
+++ carRace.cpp
@@ -69,71 +69,140 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	int fuelY = 0;
 	int imgFuel = LoadGraph("image/fuel.png");
 
+	// ゲーム進行に関する変数
+	enum { TITLE, PLAY, OVER };
+	int scene = TITLE;
+	int timer = 0;
+
+	// サウンドの読み込みと音量設定
+	int bgm = LoadSoundMem("sound/bgm.mp3");
+	int jin = LoadSoundMem("sound/gameover.mp3");
+	int seFuel = LoadSoundMem("sound/fuel.mp3");
+	int seCrash = LoadSoundMem("sound/crash.mp3");
+	ChangeVolumeSoundMem(128, bgm);
+	ChangeVolumeSoundMem(128, jin);
+
 	while (1) // メインループ
 	{
 		ClearDrawScreen(); // 画面をクリアする
 
 		// 背景のスクロール処理
-		bgY = bgY + 10;
-		if (bgY >= HEIGHT) bgY = bgY - HEIGHT;
+		if (scene == PLAY) bgY = (bgY + 10) % HEIGHT; // プレイ中にだけスクロール
 		DrawGraph(0, bgY - HEIGHT, imgBG, FALSE);
 		DrawGraph(0, bgY, imgBG, FALSE);
 
 		// プレイヤーの車を動かす処理
-		GetMousePoint(&playerX, &playerY);
-		if (playerX < 260) playerX = 260;
-		if (playerX > 460) playerX = 460;
-		if (playerY < 40) playerY = 40;
-		if (playerY > 600) playerY = 600;
+		if (scene == PLAY) // プレイ中にだけ動かす
+		{
+			GetMousePoint(&playerX, &playerY);
+			if (playerX < 260) playerX = 260;
+			if (playerX > 460) playerX = 460;
+			if (playerY < 40) playerY = 40;
+			if (playerY > 600) playerY = 600;
+		}
 		drawCar(playerX, playerY, playerType);
 
 		// コンピューターの車を動かす処理
 		for (int i = 0; i < COM_MAX; i++)
 		{
-			computerY[i] = computerY[i] + 1 + i;
-			// 画面の下から外に出たかを判定
-			if (computerY[i] > HEIGHT + 100)
+			if (scene == PLAY) // プレイ中の車の処理
 			{
-				computerX[i] = rand() % 180 + 270;
-				computerY[i] = -100;
-				computerType[i] = YELLOW + rand() % 3;
-				computerFlag[i] = 0;
+				computerY[i] = computerY[i] + 1 + i;
+				// 画面の下から外に出たかを判定
+				if (computerY[i] > HEIGHT + 100)
+				{
+					computerX[i] = rand() % 180 + 270;
+					computerY[i] = -100;
+					computerType[i] = YELLOW + rand() % 3;
+					computerFlag[i] = 0;
+				}
+				// ヒットチェック
+				int dx = abs(computerX[i] - playerX); // X軸方向のピクセル数
+				int dy = abs(computerY[i] - playerY); // Y軸方向のピクセル数
+				int wid = CAR_W[playerType] / 2 + CAR_W[computerType[i]] / 2 - 4;
+				int hei = CAR_H[playerType] / 2 + CAR_H[computerType[i]] / 2 - 4;
+				if (dx < wid && dy < hei) // 接触しているか
+				{
+					int col = GetColor(rand() % 256, rand() % 256, rand() % 256); // 重ねる色
+					SetDrawBlendMode(DX_BLENDMODE_ADD, 255); // 色を加算する設定
+					DrawBox(playerX - CAR_W[playerType] / 2, playerY - CAR_H[playerType] / 2, playerX + CAR_W[playerType] / 2, playerY + CAR_H[playerType] / 2, col, TRUE);
+					SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0); // 通常の描画に戻す
+					PlaySoundMem(seCrash, DX_PLAYTYPE_BACK); // 効果音
+					fuel -= 10;
+				}
+				// 追い抜いたかを判定
+				if (computerY[i] > playerY && computerFlag[i] == 0)
+				{
+					computerFlag[i] = 1;
+					score += 100;
+					if (score > highScore) highScore = score;
+				}
 			}
-			// ヒットチェック
-			int dx = abs(computerX[i] - playerX); // X軸方向のピクセル数
-			int dy = abs(computerY[i] - playerY); // Y軸方向のピクセル数
-			int wid = CAR_W[playerType] / 2 + CAR_W[computerType[i]] / 2 - 4;
-			int hei = CAR_H[playerType] / 2 + CAR_H[computerType[i]] / 2 - 4;
-			if (dx < wid && dy < hei) // 接触しているか
+			else  // タイトル画面とゲームオーバー画面での車の動き
 			{
-				int col = GetColor(rand() % 256, rand() % 256, rand() % 256); // 重ねる色
-				SetDrawBlendMode(DX_BLENDMODE_ADD, 255); // 色を加算する設定
-				DrawBox(playerX - CAR_W[playerType] / 2, playerY - CAR_H[playerType] / 2, playerX + CAR_W[playerType] / 2, playerY + CAR_H[playerType] / 2, col, TRUE);
-				SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0); // 通常の描画に戻す
-				fuel -= 10;
-			}
-
-			// 追い抜いたかを判定
-			if (computerY[i] > playerY && computerFlag[i] == 0)
-			{
-				computerFlag[i] = 1;
-				score += 100;
-				if (score > highScore) highScore = score;
+				computerY[i] = computerY[i] - 1 - i;
+				if (computerY[i] < -100) computerY[i] = HEIGHT + 100;
 			}
 			drawCar(computerX[i], computerY[i], computerType[i]);
 		}
 
 		// 燃料アイテムの処理
-		fuelY += 4;
-		if (fuelY > HEIGHT) fuelY = -100;
-		if (abs(fuelX - playerX) < CAR_W[playerType] / 2 + 12 && abs(fuelY - playerY) < CAR_H[playerType] / 2 + 12)
+		if (scene == PLAY) // ゲーム中だけ出現
 		{
-			fuelX = rand() % 180 + 270;
-			fuelY = -500;
-			fuel += 200;
+			fuelY += 4;
+			if (fuelY > HEIGHT) fuelY = -100;
+			if (abs(fuelX - playerX) < CAR_W[playerType] / 2 + 12 && abs(fuelY - playerY) < CAR_H[playerType] / 2 + 12)
+			{
+				fuelX = rand() % 180 + 270;
+				fuelY = -500;
+				fuel += 200;
+				PlaySoundMem(seFuel, DX_PLAYTYPE_BACK); // 効果音
+			}
+			DrawGraph(fuelX - 12, fuelY - 12, imgFuel, TRUE);
 		}
-		DrawGraph(fuelX -12, fuelY - 12, imgFuel, TRUE);
 
+		timer++; // タイマーをカウント
+		switch (scene) // タイトル、ゲームプレイ、ゲームオーバーの分岐
+		{
+		case TITLE: // タイトル画面の処理
+			drawText(160, 160, 0xffffff, "Car Race", 0, 100);
+			if (timer % 60 < 30) drawText(210, 400, 0x00ff00, "Click to start.", 0, 40);
+			if (GetMouseInput() & MOUSE_INPUT_LEFT)
+			{
+				playerX = WIDTH / 2;
+				playerY = HEIGHT / 2;
+				for (int i = 0; i < COM_MAX; i++)
+				{
+					computerY[i] = HEIGHT + 100;
+					computerFlag[i] = 0;
+				}
+				fuelX = WIDTH / 2;
+				fuelY = -100;
+				score = 0;
+				fuel = 1000;
+				scene = PLAY;
+				PlaySoundMem(bgm, DX_PLAYTYPE_LOOP); // BGMをループ再生
+			}
+			break;
+
+		case PLAY: // ゲームをプレイする処理
+			fuel -= 1;
+			if (fuel < 0)
+			{
+				fuel = 0;
+				scene = OVER;
+				timer = 0;
+				StopSoundMem(bgm); // BGMを停止
+				PlaySoundMem(jin, DX_PLAYTYPE_BACK); // ジングルを出力
+			}
+			break;
+
+		case OVER: // ゲームオーバーの処理
+			drawText(180, 240, 0xff0000, "GAME OVER", 0, 80);
+			if (timer > 60 * 5) scene = TITLE;
+			break;
+		}
+		
 		// スコアなどの表示
 		drawText(10, 10, 0x00ffff, "SCORE %d", score, 30);
 		drawText(WIDTH - 200, 10, 0xffff00, "HI-SC %d", highScore, 30);
